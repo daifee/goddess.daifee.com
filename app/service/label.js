fd3f61c30173f684adadda2241eb47a19070ef8c
daifee <mr_zhangjiayong@163.com>
2018-08-17 14:42:06 +0800

    实现标签接口

--- app/service/label.js
+++ app/service/label.js
@@ -4,6 +4,41 @@ const { Service } = require('egg');
 
 class LabelService extends Service {
 
+  async create(model) {
+    const { Label } = this.ctx.model;
+    const label = await Label.create(model);
+    return label;
+  }
+
+  async update(id, model) {
+    const { Label } = this.ctx.model;
+
+    const query = Label.find({})
+      .select('name description status')
+      .findOneAndUpdate({ _id: id }, model, { new: true });
+
+    const label = await query.exec();
+
+    return label;
+  }
+
+  async delete(id) {
+    const { Label } = this.ctx.model;
+
+    return await this.update(id, new Label({ status: 'deleted' }));
+  }
+
+  /**
+   * 查找所有标签
+   * @return {Array} 标签集合
+   */
+  async find() {
+    const { Label } = this.ctx.model;
+    const query = Label.find({})
+      .find({ status: { $ne: 'deleted' } });
+
+    return await query.exec();
+  }
 }
 
 module.exports = LabelService;
