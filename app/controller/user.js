613bf3f53562332032e367c86d2459fca737c337
daifee <mr_zhangjiayong@163.com>
2018-09-02 16:33:05 +0800

    分页展示自己的内容

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const url = require('url');
 const ViewController = require('../core/view-controller');
+
 
 class UserController extends ViewController {
 
@@ -9,9 +11,30 @@ class UserController extends ViewController {
       return this.ctx.redirect('/login');
     }
 
+    const { microBlog } = this.ctx.service;
+    const page = this.page;
+    const list = await microBlog.findByUserId(this.user.id, page, this.perPage);
+
+
+    const prevPage = page - 1;
+    const nextPage = page + 1;
+    const request = this.ctx.request;
+    const prevPageUrl = prevPage < 1 ? '' : url.format({
+      pathname: request.path,
+      query: Object.assign({}, request.query, { page: prevPage }),
+    });
+    const nextPageUrl = url.format({
+      pathname: request.path,
+      query: Object.assign({}, request.query, { page: nextPage }),
+    });
+
     await this.render('user-profile', {
       title: '用户主页',
       activeMenuKey: 'profile',
+      list,
+      page,
+      prevPageUrl,
+      nextPageUrl,
     });
   }
 
