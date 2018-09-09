74c8d08d5a72d9f95659e5ad95dbcf7b93e01bed
daifee <mr_zhangjiayong@163.com>
2018-09-09 23:17:31 +0800

    增加precommit

--- app/assets/page/render.js
+++ app/assets/page/render.js
@@ -5,14 +5,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default function render(PageComponent, props = {}, cb) {
-    ReactDOM.render(
-        <PageComponent {...props} />,
-        document.getElementById('app'),
-        cb
-    );
+  ReactDOM.render(
+    <PageComponent {...props} />,
+    document.getElementById('app'),
+    cb
+  );
 }
 
 export function hideInitLoading() {
-    const node = document.getElementById('init-loading');
-    node.remove();
+  const node = document.getElementById('init-loading');
+  node.remove();
 }
